15638fdb5c116d4efc51b26df1c78bdc02f0a404
Owner <Owner>
2016-09-19 16:17:44 -0400

    try with php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'consumer_key' => '3RdCIpqzpCTEaBVUYxTAGROVv',
+	'consumer_secret' => 'V7CjSpMW7tlIOzyOvfWPiWqPwxzSdbRK5hay5nDgExPQOy8d34',
+	'url_login' => 'http://localhost:8080/twitter_login.php',
+	'url_callback' => 'http://localhost:8080/twitter_callback.php',
+];
+?>
